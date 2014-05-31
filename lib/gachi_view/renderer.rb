205f460c90e1bf4689887a727486fc3aabaf3161
ohta <ohta@raw-hide.co.jp>
2014-05-31 14:01:55 +0900

    add example renderer

---
+++ lib/gachi_view/renderer.rb
@@ -0,0 +1,18 @@
+# = Rendering Gachi erb
+#
+module GachiView
+  module Renderer
+
+    # = rendering assigns attribute by gachi record
+    # [Usage]
+    #   extend GachiView::Renderer
+    #
+    #   render [template_path], assign_attribute
+    class << self
+      def render
+        #TODO: called ERB
+      end
+    end
+  end
+
+end
